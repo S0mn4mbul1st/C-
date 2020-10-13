7df4f9a0950ccc6d23a16df6735b80c351f38f1b
Aykhan Imanov <42781916+S0mn4mbul1st@users.noreply.github.com>
2020-10-13 10:43:15 +0200

    Add files via upload

---
+++ Tree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace tree
+{
+    class Program
+    {
+        public static void PrintTree(int n)
+        { 
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (j < n - i - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("X");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(true);
+        }
+        
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt16(Console.ReadLine());
+    
+            PrintTree(n);
+        }
+    }
+}
